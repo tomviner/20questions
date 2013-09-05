54abfad0c47f6291b37f79af1c60f4323cc2a51e
TomV <tom@viner.tv>
2013-09-05 21:28:17 +0100

    so works

--- twenty.py
+++ twenty.py
@@ -1,13 +1,5 @@
-
-QUESTIONS = (
-    "Is it an animal?",
-)
-
-OBJECTS = (
-    "Cat",
-)
-
-db = {}
+import random
+import pickle
 
 def get_answer(q):
     print q
@@ -20,6 +12,68 @@ def get_answer(q):
             if ans in 'yn':
                 return ans == 'y'
 
-for q in QUESTIONS:
-    for obj in OBJECTS:
-        db[(q, obj)] = get_answer(q)
+def final_question(obj):
+    if get_answer('Is it %s?' % obj):
+        print 'Success'
+    else:
+        interogation(obj)
+
+def interogation(obj):
+    print 'Give me a question which differentiate between ' + obj + ' and your answer'
+    question = raw_input()
+    print 'Now tell me what it is'
+    obj = raw_input()
+    ans = get_answer('And would you have answered yes or no')
+    db[(question, obj)] = ans
+    for q, a in answers.items():
+        db[(q, obj)] = a
+    print db
+    pickle.dump(db, open('db.dojo', 'w'))
+
+# not eval
+db = pickle.load(open('db.dojo'))
+
+qs = {q for (q, obj) in db}
+objs = {obj for (q, obj) in db}
+answers = {}
+
+for i, question in enumerate(qs):
+    answer = get_answer(question)
+    answers[question] = answer
+    not_objs = [obj for (q, obj), ans in db.items() if q==question and ans!=answer]
+    for obj in not_objs:
+        objs.remove(obj)
+
+    if len(objs) == 1 or i==19:
+        final_question(obj)
+        # print 'IT IS ' + obj
+        break
+    print 'it could be', objs
+else:
+    print "I ran out of questions"
+    if objs:
+        for obj in objs:
+            interogation(obj)
+    else:
+        print "And I also ran out of objects :( I've failed"
+        interogation(random.choice([obj for (q, obj) in db]))
+
+
+
+
+
+
+
+db[(q, obj)]
+# 0: 'Y'
+temp_db = db
+# number of question and
+
+# for q in QUESTIONS:
+#     for obj in OBJECTS:
+#         db[(q, obj)] = get_answer(q)
+
+# print db
+
+def pick_qestion():
+    return
